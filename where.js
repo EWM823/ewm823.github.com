172c33da4a4827a759c884b183eba8cb5dd0921f
EWM823 <Emmett.Moore@tufts.edu>
2013-03-02 19:48:18 -0500

    small bug fix

--- where.js
+++ where.js
@@ -53,26 +53,26 @@
 				});
 
 				// Calling Google Places API
-				var request = {
-					location: me,
-					radius: '500',
-					types: ['food']
-				};
-				service = new google.maps.places.PlacesService(map);
-				service.search(request, callback);
-			}
+				//var request = {
+				//	location: me,
+				//	radius: '500',
+				//	types: ['food']
+				//};
+				//service = new google.maps.places.PlacesService(map);
+				//service.search(request, callback);
+			//}
 
 			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
-			function callback(results, status)
-			{
-				if (status == google.maps.places.PlacesServiceStatus.OK) {
-					alert("Got places back!");
-					places = results;
-					for (var i = 0; i < results.length; i++) {
-						createMarker(results[i]);
-					}
-				}
-			}
+			//function callback(results, status)
+			//{
+			//	if (status == google.maps.places.PlacesServiceStatus.OK) {
+			//		alert("Got places back!");
+			//		places = results;
+			//		for (var i = 0; i < results.length; i++) {
+			//			createMarker(results[i]);
+			//		}
+			//	}
+			//}
 
 			function createMarker(place)
 			{
@@ -88,3 +88,4 @@
 					infowindow.open(map, this);
 				});
       		}
+      		
