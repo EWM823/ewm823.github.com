4483c6f7abd3a71c3dd3a47f96f45a40bc3b7575
EWM823 <Emmett.Moore@tufts.edu>
2013-03-02 20:05:38 -0500

    set up parse of mbta times

--- where.js
+++ where.js
@@ -16,7 +16,22 @@
 			{
 				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 				getMyLocation();
+				request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", true);
+				request.send(null);
+                request.onreadystatechange = parse;
+
 			}
+			
+			function parse()
+			{
+			if (request.status == 0) {
+                    alert("file failed to load");
+                }
+                if (request.readyState==4 && request.status==200) {
+                var str = request.responseText;
+                parsed = JSON.parse(str);
+                }
+            }
 
 			function getMyLocation()
 			{
@@ -52,27 +67,7 @@
 					infowindow.open(map, marker);
 				});
 
-				// Calling Google Places API
-				//var request = {
-				//	location: me,
-				//	radius: '500',
-				//	types: ['food']
-				//};
-				//service = new google.maps.places.PlacesService(map);
-				//service.search(request, callback);
 			}
-
-			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
-			//function callback(results, status)
-			//{
-			//	if (status == google.maps.places.PlacesServiceStatus.OK) {
-			//		alert("Got places back!");
-			//		places = results;
-			//		for (var i = 0; i < results.length; i++) {
-			//			createMarker(results[i]);
-			//		}
-			//	}
-			//}
 
 			function createMarker(place)
 			{
