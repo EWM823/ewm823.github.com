3222e4384715dc8f67330ec7dfd9f9f12540756b
EWM823 <Emmett.Moore@tufts.edu>
2013-03-03 17:25:48 -0500

    parsed stations

--- where.js
+++ where.js
@@ -21,7 +21,9 @@
 			var places;
 			var stations = {"RALE": [42.395428, -71.142483, "Alewife Station"],"RDAV": [42.39674, -71.121815, "Davis Square"],"RPOR": [42.3884, -71.119149, "Porter Square "],"RHAR": [42.373362, -71.118956, "Harvard Square"],"RCEN": [42.365486, -71.103802, "Central Square"],"RKEN": [42.36249079, -71.08617653, "Kendall/MIT"],"RMGH": [42.361166, -71.070628, "Charles/MGH"],"RPRK": [42.35639457, -71.0624242,"Park Street"],"RDTC": [42.355518, -71.060225, "Downtown Crossing"],"RSOU": [42.352271, -71.055242, "South Station"],"RBRO": [42.342622, -71.056967, "Broadway"],"RAND": [42.330154, -71.057655, "Andrew"],"RJFK": [42.320685, -71.052391, "JFK/UMass"],"RSAV": [42.31129, -71.053331, "Savin Hill"],"RFIE": [42.300093, -71.061667, "Fields Corner"],"RSHA": [42.29312583, -71.06573796, "Shawmut"],"RASH":[42.284652, -71.064489,"Ashmont"],"RNQU": [42.275275, -71.029583, "North Quincy"],"RWOL": [42.2665139, -71.0203369,"Wollaston"],"RQUC": [42.251809, -71.005409, "Quincy Center"],"RQUA":[42.233391, -71.007153, "Quincy Adams"],"RBRA": [42.2078543, -71.0011385, "Braintree"]};
 			var stations_marker = [];
+			var w_and_c_marker = [];
 			var stations_iw = [];
+			var w_and_c_if = [];
 			//var stations_line = [];
 			
 			function init()
@@ -64,28 +66,29 @@
             }
             function plot_w_and_c()
             {
-    			var waldo_marker = new google.map.Marker({
-					position: waldo_coord,
-					title: parsed_w_and_c[0].loc.note,
-					icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot/png'
-				});
-				waldo_marker.setMap(map);
-				carmen_marker = new google.map.Marker({
-					position: carmen_coord,
-					title: parsed_w_and_c[1].loc.note,
-					icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot/png'
-				});
-				carmen_marker.setMap(map);
-				google.map.event.addListener(waldo_marker, 'click', function() {
-					infowindow.close();
-					infowindow.setContent(waldo_marker.title);
-					infowindow.open(map, this);
-				});
-				google.map.event.addListener(carmen_marker, 'click', function() {
-					infowindow.close();
-					infowindow.setContent("Carmen Sandiego:\n" + carmen_marker.title);
-					infowindow.open(map, this);
-				});
+            	var curr_marker;           	
+				var curr_coords; //curr_stations
+            	for (var i in parsed_w_and_c) {
+					
+     		       	curr_coords = new google.maps.LatLng(stations[i][0], stations[i][1]);        		        		
+	           		curr_marker = new google.maps.Marker({
+    	       			position: curr_coords,
+        	   			title: parsed_w_and_c[i].loc.note,
+            			icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+            		});
+            		curr_marker.setMap(map);
+            		w_and_c_marker.push(curr_marker);
+            		
+            		
+            			w_and_c_marker[i] = curr_marker;  		
+	            		google.maps.event.addListener(stations_marker[i], 'click', (function(i) {
+							return function () {
+								w_and_c_iw[i] = new google.maps.InfoWindow({content: w_and_c_marker[i].title});
+    	        				w_and_c_iw[i].setContent(w_and_c_marker[i].title);
+        	    				w_and_c_iw[i].open(map, w_and_c_marker[i]);
+        	    			}
+            			})(i));
+            	}
             }
           
           	
