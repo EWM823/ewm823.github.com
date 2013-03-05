3e6fc1a0dc44234674547ce534fc948c2c382890
EWM823 <Emmett.Moore@tufts.edu>
2013-03-05 03:01:29 -0500

    ya

--- where.js
+++ where.js
@@ -108,7 +108,7 @@ function plot_stations() {
    		stations_marker.push(curr_marker);
  		stations_marker[i] = curr_marker;  		
    		google.maps.event.addListener(stations_marker[i], 'click', function() {
-				updateSTimes()
+				updateSTimes();
 				infowindow.setContent(this.title + cl_str);
 				infowindow.open(map, this);
 				
