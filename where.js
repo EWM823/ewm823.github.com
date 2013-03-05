ad2a5e5fe1cb2a50d96f556fa235099a6fd976c1
EWM823 <Emmett.Moore@tufts.edu>
2013-03-04 20:33:31 -0500

    ya

--- where.js
+++ where.js
@@ -1,31 +1,32 @@
-var myLat = 0;
+var myLat = 0;		//my coordinates
 var myLng = 0;
-var waldo_coord;
-var carmen_coord;
-var waldo_marker;
-var carmen_marker;
+var waldo_coord;		//struct of waldo's LatLng
+var carmen_coord;		//struct of carmen's LatLng
+var waldo_marker;		//marker of waldo's location
+var carmen_marker;		//marker of carmen's location
+
 var request_sched = new XMLHttpRequest();
 var request_w_and_c = new XMLHttpRequest();
 
-var parsed_w_and_c;
+var parsed_w_and_c;		//parsed JSON object for waldo and carmen
 			
-var me = new google.maps.LatLng(myLat, myLng);
+var me = new google.maps.LatLng(myLat, myLng);	//my coordinates
 var myOptions = {
-	zoom: 13, // The larger the zoom number, the bigger the zoom
+	zoom: 11, // The larger the zoom number, the bigger the zoom
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-var map;
-var marker_me;
-var infowindow = new google.maps.InfoWindow();
+var map;										//the map
+var marker_me;									//my marker
+var infowindow = new google.maps.InfoWindow();	//the one infowindow that shows up
 var places;
+// the struct of all the stations
 var stations = {"RALE": [42.395428, -71.142483, "Alewife Station"],"RDAV": [42.39674, -71.121815, "Davis Square"],"RPOR": [42.3884, -71.119149, "Porter Square "],"RHAR": [42.373362, -71.118956, "Harvard Square"],"RCEN": [42.365486, -71.103802, "Central Square"],"RKEN": [42.36249079, -71.08617653, "Kendall/MIT"],"RMGH": [42.361166, -71.070628, "Charles/MGH"],"RPRK": [42.35639457, -71.0624242,"Park Street"],"RDTC": [42.355518, -71.060225, "Downtown Crossing"],"RSOU": [42.352271, -71.055242, "South Station"],"RBRO": [42.342622, -71.056967, "Broadway"],"RAND": [42.330154, -71.057655, "Andrew"],"RJFK": [42.320685, -71.052391, "JFK/UMass"],"RSAV": [42.31129, -71.053331, "Savin Hill"],"RFIE": [42.300093, -71.061667, "Fields Corner"],"RSHA": [42.29312583, -71.06573796, "Shawmut"],"RASH":[42.284652, -71.064489,"Ashmont"],"RNQU": [42.275275, -71.029583, "North Quincy"],"RWOL": [42.2665139, -71.0203369,"Wollaston"],"RQUC": [42.251809, -71.005409, "Quincy Center"],"RQUA":[42.233391, -71.007153, "Quincy Adams"],"RBRA": [42.2078543, -71.0011385, "Braintree"]};
-var stations_marker = [];
-var w_and_c_marker = [];
-var stations_iw = [];
-var w_and_c_iw = [];
-var stations_coords = [];
-var infowindow = new google.maps.InfoWindow();
+var stations_marker = [];	//array of station markers
+var w_and_c_marker = [];	// array of up waldo's and/or/nor carmen's markers
+var stations_iw = [];		// DELETE THIS--ARRAY OF INFO WINDOWS IS UNNECESARRY
+var w_and_c_iw = [];		// DELETE THIS--ARRAY OF INFO WINDOWS IS UNNECESARRY
+var stations_coords = [];	// Array of stations coordinates
 			
 function init()
 {
